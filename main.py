fa9db376dec170ceafae920c1d52375cbfcc2486
smellerbee <115466602+smellerbee1@users.noreply.github.com>
2022-10-10 11:50:30 +0000

    2 uzd

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-
-sk1=int(input("ievadirtskaitli: "))
-if sk1>10:
-  print("skaitlis ir liels")
+from math import *
+a=int(input("Ievadiet a:"))
+b=int(input("Ievadiet b:"))
+c=int(input("Ievadiet c:"))
+D=b**2-4*a*c
+if D<0:
+  print("Saknu nav!")
+elif D==0:
+  x=-b/2*a
+  print("Ir viena sakne =",x)
 else:
-  print("skaitlis ir mazs")
+  x1=(-b+sqrt(D))/2*a
+  x2=(-b-sqrt(D))/2*a
+print("Ir divas saknes, x1 =",x1," x2 =",x2)
